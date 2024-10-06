7404d9000afa38b4e942bb6309d7f88e2160ed44
theDAOS <sanjusanthosh46@gmail.com>
2024-10-06 12:34:54 +0530

    upload_file view added

--- LMS/instructor/views.py
+++ LMS/instructor/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.models import User
-from homeapp.models import User_types
 from django.contrib.auth.decorators import login_required
+from homeapp.models import User_types
+from .forms import FileUploadForm
+
 
 @login_required(login_url="/login/")
 def instructor_dashboard(request):
@@ -18,3 +20,18 @@ def instructor_dashboard(request):
         return HttpResponse(template.render(context, request))
     else:
         return redirect('/Redirect/')
+
+# Testing file upload
+# new_file = File.objects.create(user=user, file_path='/images/photo1.jpg', file_type='image')
+@login_required
+def upload_file(request):
+    if request.method == 'POST':
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            file_instance = form.save(commit=False)
+            file_instance.user = request.user  # Set the user
+            file_instance.save()
+            return redirect('instructor_dashboard')  # Redirect to a success page or file list
+    else:
+        form = FileUploadForm()
+    return render(request, 'upload.html', {'form': form})
